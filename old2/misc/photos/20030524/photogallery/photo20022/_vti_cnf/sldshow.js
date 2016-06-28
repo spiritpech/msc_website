4412ed9d0af16e5678d29a3160a86a1aadc18642
dwai213 <dwai213@gmail.com>
2016-06-28 00:58:27 -0700

    Added old pages

---
+++ old2/misc/photos/20030524/photogallery/photo20022/_vti_cnf/sldshow.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|MSC_LAPTOP\\Chun-chih
+vti_modifiedby:SR|MSC_LAPTOP\\Chun-chih
+vti_timelastmodified:TR|29 May 2003 07:17:17 -0000
+vti_timecreated:TR|29 May 2003 07:17:17 -0000
+vti_lineageid:SR|{40447B93-F8E1-4EAC-ACB2-731C836B078E}
+vti_cacheddtm:TX|29 May 2003 07:17:17 -0000
+vti_filesize:IR|4362
+vti_extenderversion:SR|5.0.2.2623
+vti_backlinkinfo:VX|photogallery/photo20022/real.htm
---
+++ old2/misc/photos/2003ACC/photogallery/photo8945/_vti_cnf/sldshow.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|13 Jun 2003 01:15:53 -0000
+vti_author:SR|MSC_LAPTOP\\Chun-chih
+vti_modifiedby:SR|MSC_LAPTOP\\Chun-chih
+vti_nexttolasttimemodified:TW|13 Jun 2003 01:15:53 -0000
+vti_timecreated:TR|13 Jun 2003 01:16:10 -0000
+vti_lineageid:SR|{FCE0C38B-89FE-4BAD-9C8D-66D2E3E9D912}
+vti_cacheddtm:TX|13 Jun 2003 01:16:10 -0000
+vti_filesize:IR|4362
+vti_extenderversion:SR|5.0.2.2623
+vti_backlinkinfo:VX|photogallery/photo8945/real.htm
